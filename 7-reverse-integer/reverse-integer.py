74cf2947c99ae2be5c3623a54e56435dc1416cc4
dineshsainichuru <dineshsaini12213@gmail.com>
2026-09-04 17:22:26 +0530

    Time: 28 ms (5.61%) | Memory: 12.4 MB (52.03%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -5,10 +5,9 @@ class Solution(object):
         if x<0:
             sign = -1
             x = x*-1
-        num = 0
-        while(x != 0):
-            num = num*10 + x%10
-            x = x//10
+        num = str(abs(x))
+        num = num[::-1]
+        num = int(num)
         num = num*sign
 
         if num < (-2**31) or num > ((2**31)-1):
