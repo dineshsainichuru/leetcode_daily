3e39894ea146d6ae95e6bb42c033cb1ba1ce3f0f
dineshsainichuru <dineshsaini12213@gmail.com>
2026-09-06 01:05:10 +0530

    Time: 243 ms (12.54%) | Memory: 13 MB (55.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,7 +2,7 @@ class Solution(object):
     def isPalindrome(self, s):
         new = ""
         for char in s :
-            if char.isalpha() or char.isnumeric():
+            if char.isalnum():
                 new = new+char.lower()
         newnew = new[::-1]
         if new == newnew:
